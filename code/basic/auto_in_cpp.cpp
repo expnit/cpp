1058d9ef17eeefc80164582009661fb04891afe7
expnit <expnit@gmail.com>
2023-11-08 14:46:34 +0530

    auto in C++

---
+++ code/basic/auto_in_cpp.cpp
@@ -0,0 +1,27 @@
+
+#include <iostream>
+#include <string>
+
+int main()
+{
+    using std::cin;
+    using std::cout;
+    using std::endl;
+    using std::string;
+
+    auto is_cpp_good_programming = true;
+
+    auto temprature_of_antarctica = -45;
+
+    auto user_name = "Chauhan Pruthviraj";
+
+    cout << "==========================================" << endl;
+
+    cout << "IS C++ Programming Good ? " << is_cpp_good_programming << endl;
+
+    cout << "Temprature of Antarctica : " << temprature_of_antarctica << endl;
+
+    cout << "My Name is : " << user_name << endl;
+
+    return 0;
+}
