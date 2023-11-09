019810d63c58e278fb9ab3f087568cd0d55f7ea5
expnit <expnit@gmail.com>
2023-11-09 14:02:02 +0530

    added more example

--- code/array/array_1.cpp
+++ code/array/array_1.cpp
@@ -1,17 +1,19 @@
 #include <iostream>
-
+#include <string>
 int main()
 {
     using std::cout;
     using std::endl;
-
+    using std::string;
     const unsigned short int length = 5;
+
+    string elements[length] = {"First", "Second", "Third", "Fourth", "Fifth"};
 
     unsigned short int array[length] = {1, 2, 3, 4, 5};
 
     for (unsigned short int index = 0; index < length; index++)
     {
-        cout << "Array [" << index + 1 << "] = " << array[index] << endl;
+        cout << elements[index] << " Element at index [" << index + 1 << "] = " << array[index] << endl;
     }
 
     return 0;
